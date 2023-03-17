7301bea7093bfd2bd61a3ca0bcc9716af5ec0c26
Ivan <omg.dayz@gmail.com>
2023-03-18 03:55:42 +0700

    upd: fake base migrations

--- alembic_fake/models.py
+++ alembic_fake/models.py
@@ -1,9 +1,17 @@
-from sqlalchemy import Column, Integer, String
+import sys
+from inspect import getsourcefile
+from os.path import abspath
+from pathlib import Path
+
+from sqlalchemy import Column, Integer, String, Boolean
+
+sys.path.append(str(Path(__file__).parents[1]))
+sys.path.append("..")
 
 from fake_database import Base_fake
 
 
-class Loan(Base_fake):
+class FakeUser(Base_fake):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True)
     last_name = Column(String, nullable=False)
@@ -12,4 +20,4 @@ class Loan(Base_fake):
     passport = Column(String, nullable=False)
     period = Column(Integer, nullable=False)
     amount = Column(Integer, nullable=False)
-    status = Column(String, nullable=False)
+    is_active = Column(Boolean)
