6271e233677e33c026a791a46f0acb657bebc6d0
Ivan <omg.dayz@gmail.com>
2023-03-25 13:45:48 +0700

    add: React frontend

--- frontend/src/components/Accountant.js
+++ frontend/src/components/Accountant.js
@@ -3,95 +3,97 @@ import {useNavigate} from 'react-router-dom';
 import {accountant, logout, pay} from '../APIs';
 
 function Accountant() {
-    const navigate = useNavigate();
-    const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        accountant()
-            .then((response) => response.json())
-            .then((result) => {
-                setUsers(result.data);
-            });
-    }, [users]);
+  useEffect(() => {
+    accountant()
+        .then((response) => response.json())
+        .then((res) => {
+          setUsers(res.data);
+        });
+  }, []);
 
-    const handleLogout = async () => {
-        const response = await logout();
-        const res = await response.json();
-        if (response.ok == true) {
-            navigate('/login');
-        } else {
-            alert(res.detail);
-        }
-    };
-    const handleSubmit = async (id) => {
-        const response = await pay(id);
-        const res = await response.json();
-        alert(res.detail);
-    };
-    return (
-        <>
-            <nav className='bg-orange-300 border-gray-200 px-4 lg:px-6 py-2.5'>
-                <div className='flex flex-wrap justify-end items-center mx-auto max-w-screen-xl'>
-                    <button
-                        onClick={handleLogout}
-                        className='bg-orange-500 hover:bg-orange-400 text-white font-bold py-2 px-4 border-b-4 border-orange-700 hover:border-orange-500 rounded mr-2 w-1/6 text-center'
-                    >
-                        Log Out
-                    </button>
-                </div>
-            </nav>
-            <div className='bg-orange-200 min-h-screen'>
-                <h2 className='text-2xl font-bold text-center text-white pt-10 mb-5'>
-                    Accountant menu
-                </h2>
-                {users.length == 0 ? null : (
-                    <div className='overflow-x-auto  sm:rounded-lg mx-5 mb-10'>
-                        <table className='w-full text-sm text-left text-white '>
-                            <thead className='text-xs text-white uppercase bg-orange-500'>
-                            <tr>
-                                <th className='px-6 py-3'>Creation Date</th>
-                                <th className='px-6 py-3'>End Date</th>
-                                <th className='px-6 py-3'>Amount</th>
-                                <th className='px-6 py-3'>Period</th>
-                                <th className='px-6 py-3'>Status</th>
-                                <th className='px-6 py-3'>First Name</th>
-                                <th className='px-6 py-3'>Last Name</th>
-                                <th className='px-6 py-3'>Middle Name</th>
-                                <th className='px-6 py-3'>Passport</th>
-                                <th className='px-6 py-3'>Pay</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {users.map((user) => (
-                                <tr key={user.id} className='bg-orange-300'>
-                                    <td className='px-6 py-4'>{user.creation_date}</td>
-                                    <td className='px-6 py-4'>{user.end_date}</td>
-                                    <td className='px-6 py-4'>{user.amount}</td>
-                                    <td className='px-6 py-4'>{user.period}</td>
-                                    <td className='px-6 py-4'>{user.status}</td>
-                                    <td className='px-6 py-4'>{user.user.first_name}</td>
-                                    <td className='px-6 py-4'>{user.user.last_name}</td>
-                                    <td className='px-6 py-4'>{user.user.middle_name}</td>
-                                    <td className='px-6 py-4'>{user.user.passport.number}</td>
-                                    <td className='cursor-pointer bg-green-500  ml-2 hover:bg-green-700'>
-                                        <button
-                                            className='w-full h-12'
-                                            onClick={() => {
-                                                handleSubmit(user.user.id);
-                                            }}
-                                        >
-                                            Confirm
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                            </tbody>
-                        </table>
-                    </div>
-                )}
-            </div>
-        </>
-    );
+  const handleLogout = async () => {
+    const response = await logout();
+    const res = await response.json();
+    if (response.ok == true) {
+      navigate('/login');
+    } else {
+      alert(res.detail);
+    }
+  };
+  const handleSubmit = async (id) => {
+    let response = await pay(id);
+    let res = await response.json();
+    alert(res.detail);
+    response = await accountant();
+    res = await response.json();
+    setUsers(res.data);
+  };
+  return (
+      <>
+        <nav className='bg-orange-300 border-gray-200 px-4 lg:px-6 py-2.5'>
+          <div className='flex flex-wrap justify-end items-center mx-auto max-w-screen-xl'>
+            <button
+                onClick={handleLogout}
+                className='bg-orange-500 hover:bg-orange-400 text-white font-bold py-2 px-4 border-b-4 border-orange-700 hover:border-orange-500 rounded mr-2 w-1/6 text-center'
+            >
+              Log Out
+            </button>
+          </div>
+        </nav>
+        <div className='bg-orange-200 min-h-screen'>
+          <h2 className='text-2xl font-bold text-center text-white pt-10 mb-5'>
+            Accountant menu
+          </h2>
+          {users.length == 0 ? null : (
+              <div className='overflow-x-auto  sm:rounded-lg mx-5 mb-10'>
+                <table className='w-full text-sm text-left text-white '>
+                  <thead className='text-xs text-white uppercase bg-orange-500'>
+                  <tr>
+                    <th className='px-6 py-3'>Creation Date</th>
+                    <th className='px-6 py-3'>End Date</th>
+                    <th className='px-6 py-3'>Amount</th>
+                    <th className='px-6 py-3'>Period</th>
+                    <th className='px-6 py-3'>Status</th>
+                    <th className='px-6 py-3'>First Name</th>
+                    <th className='px-6 py-3'>Last Name</th>
+                    <th className='px-6 py-3'>Middle Name</th>
+                    <th className='px-6 py-3'>Passport</th>
+                    <th className='px-6 py-3'>Pay</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  {users.map((user) => (
+                      <tr key={user.id} className='bg-orange-300'>
+                        <td className='px-6 py-4'>{user.creation_date}</td>
+                        <td className='px-6 py-4'>{user.end_date}</td>
+                        <td className='px-6 py-4'>{user.amount}</td>
+                        <td className='px-6 py-4'>{user.period}</td>
+                        <td className='px-6 py-4'>{user.status}</td>
+                        <td className='px-6 py-4'>{user.user.first_name}</td>
+                        <td className='px-6 py-4'>{user.user.last_name}</td>
+                        <td className='px-6 py-4'>{user.user.middle_name}</td>
+                        <td className='px-6 py-4'>{user.user.passport.number}</td>
+                        <td className='cursor-pointer bg-green-500  ml-2 hover:bg-green-700'>
+                          <button
+                              className='w-full h-12'
+                              onClick={() => {
+                                handleSubmit(user.user.id);
+                              }}
+                          >
+                            Confirm
+                          </button>
+                        </td>
+                      </tr>
+                  ))}
+                  </tbody>
+                </table>
+              </div>
+          )}
+        </div>
+      </>
+  );
 }
-
 export default Accountant;
--- frontend/src/components/Operator.js
+++ frontend/src/components/Operator.js
@@ -5,114 +5,117 @@ import {loadPassport, logout, operation, verify} from '../APIs';
 const baseImage = 'https://klike.net/uploads/posts/2022-06/1654842544_1.jpg';
 
 function Operator() {
-    const navigate = useNavigate();
-    const [image, setImage] = useState(baseImage);
+  const navigate = useNavigate();
+  const [image, setImage] = useState(baseImage);
 
-    const [unverifs, setUnverifs] = useState([]);
-    useEffect(() => {
-        operation()
-            .then((response) => response.json())
-            .then((res) => {
-                setUnverifs(res.data);
-            });
-    }, [unverifs]);
+  const [unverifs, setUnverifs] = useState([]);
 
-    const handleLogout = async () => {
-        const response = await logout();
-        const res = await response.json();
-        if (response.ok == true) {
-            navigate('/login');
-        } else {
-            alert(res.detail);
-            alert();
-        }
-    };
+  useEffect(() => {
+    operation()
+        .then((response) => response.json())
+        .then((res) => {
+          setUnverifs(res.data);
+        });
+  }, []);
 
-    const handleImage = async (src) => {
-        const response = await loadPassport(src);
-        const res = await response.blob();
-        setImage(URL.createObjectURL(res));
-        if (response.ok == false) {
-            alert(res.detail);
-        }
-    };
+  const handleLogout = async () => {
+    const response = await logout();
+    const res = await response.json();
+    if (response.ok == true) {
+      navigate('/login');
+    } else {
+      alert(res.detail);
+      alert();
+    }
+  };
 
-    const handleVerify = async (src) => {
-        const response = await verify(src);
-        setImage(baseImage);
-        const res = await response.json();
-        if (response.ok == false) {
-            alert(res.detail);
-        }
-    };
-    return (
-        <>
-            <nav className='bg-orange-300 border-gray-200 px-4 lg:px-6 py-2.5'>
-                <div className='flex flex-wrap justify-end items-center mx-auto max-w-screen-xl'>
-                    <button
-                        onClick={handleLogout}
-                        className='bg-orange-500 hover:bg-orange-400 text-white font-bold py-2 px-4 border-b-4 border-orange-700 hover:border-orange-500 rounded mr-2 w-1/6 text-center'
-                    >
-                        Log Out
-                    </button>
-                </div>
-            </nav>
-            <div className='bg-orange-200 min-h-screen'>
-                <h2 className='text-2xl font-bold text-center text-white pt-10 mb-5'>
-                    Operator menu
-                </h2>
-                {unverifs.length == 0 ? null : (
-                    <div className='overflow-x-auto  sm:rounded-lg mx-5'>
-                        <table className='w-full text-sm text-left text-white '>
-                            <thead className='text-xs text-white uppercase bg-orange-500'>
-                            <tr>
-                                <th className='px-6 py-3'>Name</th>
-                                <th className='px-6 py-3'>Middle Name</th>
-                                <th className='px-6 py-3'>Last Name</th>
-                                <th className='px-6 py-3'>Email</th>
-                                <th className='px-6 py-3'>Registered Date</th>
-                                <th className='px-6 py-3'>Passport</th>
-                                <th className='px-6 py-3'>Filename</th>
-                                <th className='px-6 py-3'>Verify</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {unverifs.map((unverif) => (
-                                <tr key={unverif.passport_id} className='bg-orange-400'>
-                                    <td className='px-6 py-4'>{unverif.user.first_name}</td>
-                                    <td className='px-6 py-4'>{unverif.user.middle_name}</td>
-                                    <td className='px-6 py-4'>{unverif.user.last_name}</td>
-                                    <td className='px-6 py-4'>{unverif.user.email}</td>
-                                    <td className='px-6 py-4'>{unverif.user.registered_at}</td>
-                                    <td className='px-6 py-4'>{unverif.number}</td>
-                                    <td>
-                                        <button
-                                            className='w-full h-12'
-                                            onClick={() => handleImage(unverif.filename)}
-                                        >
-                                            Check the passport
-                                        </button>
-                                    </td>
-                                    <td>
-                                        <button
-                                            className='w-full h-12'
-                                            onClick={() => handleVerify(unverif.user.user_id)}
-                                        >
-                                            Verify
-                                        </button>
-                                    </td>
-                                </tr>
-                            ))}
-                            </tbody>
-                        </table>
-                    </div>
-                )}
-                <div className='mx-auto w-1/2 mt-10'>
-                    <img className='w-full' src={image}></img>
-                </div>
-            </div>
-        </>
-    );
+  const handleImage = async (src) => {
+    const response = await loadPassport(src);
+    const res = await response.blob();
+    setImage(URL.createObjectURL(res));
+    if (response.ok == false) {
+      alert(res.detail);
+    }
+  };
+
+  const handleVerify = async (src) => {
+    let response = await verify(src);
+    setImage(baseImage);
+    let res = await response.json();
+    if (response.ok == false) {
+      alert(res.detail);
+    }
+    response = await operation();
+    res = await response.json();
+    setUnverifs(res.data);
+  };
+  return (
+      <>
+        <nav className='bg-orange-300 border-gray-200 px-4 lg:px-6 py-2.5'>
+          <div className='flex flex-wrap justify-end items-center mx-auto max-w-screen-xl'>
+            <button
+                onClick={handleLogout}
+                className='bg-orange-500 hover:bg-orange-400 text-white font-bold py-2 px-4 border-b-4 border-orange-700 hover:border-orange-500 rounded mr-2 w-1/6 text-center'
+            >
+              Log Out
+            </button>
+          </div>
+        </nav>
+        <div className='bg-orange-200 min-h-screen'>
+          <h2 className='text-2xl font-bold text-center text-white pt-10 mb-5'>
+            Operator menu
+          </h2>
+          {unverifs.length == 0 ? null : (
+              <div className='overflow-x-auto  sm:rounded-lg mx-5'>
+                <table className='w-full text-sm text-left text-white '>
+                  <thead className='text-xs text-white uppercase bg-orange-500'>
+                  <tr>
+                    <th className='px-6 py-3'>Name</th>
+                    <th className='px-6 py-3'>Middle Name</th>
+                    <th className='px-6 py-3'>Last Name</th>
+                    <th className='px-6 py-3'>Email</th>
+                    <th className='px-6 py-3'>Registered Date</th>
+                    <th className='px-6 py-3'>Passport</th>
+                    <th className='px-6 py-3'>Filename</th>
+                    <th className='px-6 py-3'>Verify</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  {unverifs.map((unverif) => (
+                      <tr key={unverif.passport_id} className='bg-orange-400'>
+                        <td className='px-6 py-4'>{unverif.user.first_name}</td>
+                        <td className='px-6 py-4'>{unverif.user.middle_name}</td>
+                        <td className='px-6 py-4'>{unverif.user.last_name}</td>
+                        <td className='px-6 py-4'>{unverif.user.email}</td>
+                        <td className='px-6 py-4'>{unverif.user.registered_at}</td>
+                        <td className='px-6 py-4'>{unverif.number}</td>
+                        <td>
+                          <button
+                              className='w-full h-12'
+                              onClick={() => handleImage(unverif.filename)}
+                          >
+                            Check the passport
+                          </button>
+                        </td>
+                        <td>
+                          <button
+                              className='w-full h-12'
+                              onClick={() => handleVerify(unverif.user.user_id)}
+                          >
+                            Verify
+                          </button>
+                        </td>
+                      </tr>
+                  ))}
+                  </tbody>
+                </table>
+              </div>
+          )}
+          <div className='mx-auto w-1/2 mt-10'>
+            <img className='w-full' src={image}></img>
+          </div>
+        </div>
+      </>
+  );
 }
-
 export default Operator;
